aafea486452558b7a4017dac4da036b3f6bf92e2
souravgreatest <jainsourav220@gmail.com>
2025-04-29 13:43:22 +0530

    Completed assignment

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { marked } from 'marked';
 import '../styles/styles.css';
 
 const MarkdownEditor = () => {
-    const [text, setText] = useState('');
-    const [output, setOutput] = useState('');
+  const [text, setText] = useState("");
+  const [output, setOutput] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setOutput(marked.parse(text));
-    }, [text]);
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setOutput(text);
+      setLoading(false);
+    }, 0);
 
-    return (
-        <div>
-            <textarea
-                className='textarea'
-                onChange={(e) => setText(e.target.value)}
-                value={text}
-            />
-            <div
-                className='preview'
-                dangerouslySetInnerHTML={{ __html: output }}
-            />
-        </div>
-    );
-};
+    return () => clearTimeout(timer); // Cleanup on re-render
+  }, [text]);
+
+  return (
+    <div>
+      <textarea
+        className='textarea'
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      ></textarea>
+      {loading ? (
+        <h1 className='loading'>loading...</h1>
+      ) : (
+        <h1 className='preview'>{output}</h1>
+      )}
+    </div>
+  );
+}
 
 export default MarkdownEditor;
