9bb7d1823dd8eb408a0f5e513f03ba35b997c6f9
souravgreatest <jainsourav220@gmail.com>
2025-04-29 10:29:04 +0530

    Completed assignment

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { marked } from 'marked';
 import '../styles/styles.css';
 
 const MarkdownEditor = () => {
-    const [text,setText]=useState("");
-    const [output,setOutput]=useState("");
-    useEffect(()=>{
-        setOutput(text);
-    },[text])
-  return (
-    <div>
-      <textarea className='textarea' onChange={(e)=>setText(e.target.value)}></textarea>
-      <h1 className='preview'>{output}</h1>
-    </div>
-  );
-}
+    const [text, setText] = useState('');
+    const [output, setOutput] = useState('');
+
+    useEffect(() => {
+        setOutput(marked.parse(text));
+    }, [text]);
+
+    return (
+        <div>
+            <textarea
+                className='textarea'
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+            />
+            <div
+                className='preview'
+                dangerouslySetInnerHTML={{ __html: output }}
+            />
+        </div>
+    );
+};
 
 export default MarkdownEditor;
